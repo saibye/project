95dddffef24f981a56a72a52ca10abcff1e10402
fei.sai3 <fei.sai3>
2020-03-08 22:05:01 +0800

    2020-3-8

--- src/common/saimail.py
+++ src/common/saimail.py
@@ -5,6 +5,11 @@ from email import encoders
 from email.header import Header
 from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
+
+from email.message import EmailMessage
+from email.utils import make_msgid
+import mimetypes
+
 import smtplib
 import json
 import saiobj
@@ -160,6 +165,82 @@ def saimail_html(_subject, _body):
         log_info("send to mailbox: %s", to_addr)
         saimail_inner(_subject, _body, to_addr, mail_type)
 
+
+def saimail_photo_inner(_subject, _body, _photo_file_path, _to_addr):
+
+    smtp_server = sai_conf_get("smtp_server", "host")
+    from_addr   = sai_conf_get("from_addr",   "mail")
+    mail_pass   = sai_conf_get("from_addr",   "passwd")
+
+    to_addr = _to_addr
+    to_list = []
+    to_list.append(to_addr)
+
+    msg = EmailMessage()
+
+    # generic email headers
+    msg['From']     = 'SAI <%s>' % from_addr
+    msg['Subject']  = _subject
+
+    # set the plain text body
+    msg.set_content('This is a plain text body.')
+
+    # now create a Content-ID for the image
+    image_cid = make_msgid(domain='sai.com')
+    # if `domain` argument isn't provided, it will 
+    # use your computer's name
+
+    html = """\
+    <html>
+        <body>
+            <p>%s<br>
+            </p>
+            <img src="cid:{image_cid_x}">
+        </body>
+    </html>"""  % (_body)
+
+    # set an alternative html body
+    msg.add_alternative(html.format(image_cid_x=image_cid[1:-1]), subtype='html')
+    # image_cid looks like <long.random.number@sai.com>
+    # to use it as the img src, we don't need `<` or `>`
+    # so we use [1:-1] to strip them off
+
+
+    # now open the image and attach it to the email
+    with open(_photo_file_path, 'rb') as img:
+
+        # know the Content-Type of the image
+        maintype, subtype = mimetypes.guess_type(img.name)[0].split('/')
+
+        # attach it
+        msg.get_payload()[1].add_related(img.read(), 
+                                             maintype=maintype, 
+                                             subtype=subtype, 
+                                             cid=image_cid)
+
+
+    try :
+        # server = smtplib.SMTP(smtp_server, 25)
+        server = smtplib.SMTP_SSL(smtp_server, 465)
+        server.login(from_addr, mail_pass)
+        server.sendmail(from_addr, to_list, msg.as_string())
+        server.quit()
+        return 0
+    except Exception as e:
+        log_error("error: send mail failure: %s", e)
+        return -1
+
+
+
+
+
+# the message is ready now
+# you can write it to a file
+# or send it using smtplib
+
+
+
+
 def saimail(_subject, _body):
 
     mail_conf_js = analy_mail_conf()
@@ -192,6 +273,23 @@ def saimail_dev(_subject, _body):
         saimail_inner(_subject, _body, to_addr, mail_type)
 
 
+# 2020-3-8
+def saimail_photo(_subject, _body, _photo_file_path):
+
+    mail_conf_js = analy_mail_conf()
+    if mail_conf_js == -1:
+        log_error("邮箱地址配置文件解析错误!")
+        return -1
+
+    # mail_type = 'plain'
+
+    to_addr = ""
+    for item in mail_conf_js['to_addrs']:
+        to_addr = item['mail']
+        log_info("send photo to DEV: %s", to_addr)
+        saimail_photo_inner(_subject, _body, _photo_file_path, to_addr)
+
+
 if __name__=="__main__":
     sailog_set("saimail.log")
     subject   = u"xxx subject"
@@ -217,6 +315,13 @@ if __name__=="__main__":
 
     subject   = u"DEV subject你好"
     body      = u"hello, world, buy buy buy你好"
-    saimail_dev(subject, body)
+    # saimail_dev(subject, body)
+
+    subject   = u"DEV 照片"
+    body      = u"你好 photo\nnew line"
+    photo_file_path = '/home/sai3/project/src/trace/000725.png'
+    photo_file_path = '/home/sai3/project/src/trace/601066.png'
+    saimail_photo(subject, body, photo_file_path)
+
 
 # saimail.py
--- src/trace/trace.py
+++ src/trace/trace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- encoding: utf8 -*-
 
+import os
+import _thread
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -9,50 +12,67 @@ from common  import *
 from pub     import *
 
 
+g_photo_dir = './'
 
 
-def trace_plot(_uid, _stock_id, _ma_list, _db):
+def trace_plot(_date, _uid, _stock_id, _ma_list, _db):
     log_info('trace_plot: begin')
 
-    length = 500
-    sai_fmt_set_fetch_len(length)
+    global g_photo_dir
+    photo_path = '%s/%s.png' % (g_photo_dir, _stock_id)
 
-    till_date = '2020-03-06'
+    till_date = _date
 
-    # df = sai_fmt_set(_stock_id, till_date, mode, length, _db)
     df = sai_fmt_simple(_stock_id, till_date, _db)
     log_info("simple: \n%s", df)
 
     df = df.set_index('pub_date').sort_index(ascending=True)
     log_info("new: \n%s", df)
 
-    df['MA5']  	= df['close_price'].rolling(5).mean()
-    df['MA10'] 	= df['close_price'].rolling(10).mean()
+
+    plot_len = int(sai_conf_get2('boot', 'plot_len'))
+    log_info("plot-len: \n%d", plot_len)
+    df = df.tail(plot_len)
+
+
+    # This can clear context
+    fig = plt.figure()
+
+    df['close_price'].plot(label='close price', title=_stock_id, ls='-', lw=0.8, color = 'black', figsize=(10,5))
+
+    log_info("ma-list: %s", _ma_list)
+    for ma in _ma_list:
+        ma_name = 'MA%d' % (ma)
+        df[ma_name] = df['close_price'].rolling(ma).mean()
+        df[ma_name].plot(label=ma_name, lw=0.3)
+        log_info('plot: %s', ma_name)
+
+    """
     df['MA20'] 	= df['close_price'].rolling(20).mean()
     df['MA50']	= df['close_price'].rolling(50).mean()
     df['MA200'] = df['close_price'].rolling(200).mean()
 
-    df = df.tail(300)
-
-    df['close_price'].plot(label='close price', title=_stock_id, ls='-', lw=0.8, color = 'black', figsize=(10,5))
-    #df['MA5'].plot(label='MA5', color='red')
-    #df['MA10'].plot(label='MA10', color='grey')
     df['MA20'].plot(label='MA20', color='red', lw=0.2)
-    df['MA50'].plot(label='MA50', color='blue', lw=0.3)
-    df['MA200'].plot(label='MA200', color='m', lw=0.5)
-    
-    plt.xlabel('date')
-    plt.ylabel('close')
-    # plt.savefig(_stock_id+'.png', dpi=300)
-    plt.savefig(_stock_id+'.png')
-    plt.show()
+    df['MA50'].plot(label='MA50', color='green', lw=0.3)
+    df['MA200'].plot(label='MA200', color='blue', lw=0.5)
+    """
+
+    plt.savefig(photo_path, dpi=300)
+
+    # plt.show()
+
+    # email
+    subject = 'T: %s#%s' % (_stock_id, _date)
+    body = '%s, %s' % (_date, _stock_id)
+    # saimail_photo(subject, body, photo_path)
+    _thread.start_new_thread(saimail_photo, (subject, body, photo_path) )
 
     log_info('trace_plot: end')
 
     return 0
 
 
-def trace_one_stock(_uid, _stock_id, _db):
+def trace_one_stock(_date, _uid, _stock_id, _db):
     log_info('trace_one_stock: begin')
 
     log_info('id: [%s+%s]', _uid, _stock_id)
@@ -65,7 +85,7 @@ def trace_one_stock(_uid, _stock_id, _db):
         log_info('(P)error: df is None')
         return -1
     else :
-        log_info('ma length: %d', len(df))
+        log_info('df-ma length: %d', len(df))
 
     ma_list = []
     for idx, row in df.iterrows():
@@ -74,7 +94,7 @@ def trace_one_stock(_uid, _stock_id, _db):
         log_info(">>>>>> ma:\t [%s, %s, %d]", _uid, _stock_id, ma)
 
     log_info("ma-list: %s", ma_list)
-    trace_plot(_uid, _stock_id, ma_list, _db)
+    trace_plot(_date, _uid, _stock_id, ma_list, _db)
 
     log_info('trace_one_stock: end')
 
@@ -85,7 +105,7 @@ def trace_one_stock(_uid, _stock_id, _db):
 0   000725
 1   601066
 '''
-def trace_one_user(_uid, _db):
+def trace_one_user(_date, _uid, _db):
     log_info('trace_one_user: begin')
 
     log_info('uid: [%s]', _uid)
@@ -98,21 +118,53 @@ def trace_one_user(_uid, _db):
         log_info('(P)error: df is None')
         return -1
     else :
-        log_info('reg length: %d', len(df))
+        log_info('df-reg length: %d', len(df))
     
     for idx, row in df.iterrows():
         stock_id = row['stock_id']
         log_info(">>>> reg:\t [%s, %s]", _uid, stock_id)
-        trace_one_stock(_uid, stock_id, _db)
+        trace_one_stock(_date, _uid, stock_id, _db)
 
     log_info('trace_one_user: end')
 
     return 0
 
 
+def trace_get_date(_trade_date):
+
+    trade_date = ''
+
+    if len(_trade_date) == 10:
+        trade_date = _trade_date
+        log_info('use [%s] as a date', trade_date)
+    elif len(_trade_date) <= 3:
+        offset = 0 - int(_trade_date)
+        trade_date = get_date_by(offset)
+        log_info('fetch date[%s] by offset[%d]', trade_date, offset)
+    else:
+        trade_date = get_date_by(0)
+        log_info('use today', trade_date)
+
+    return trade_date
+
+
 def production(_db):
 
     log_info('(P)mode: begin')
+
+    # pd.set_option('display.max_columns', None)
+    # pd.set_option('display.max_rows', None)
+
+    # till date
+    till_date = sai_conf_get2('boot', 'till_date')
+    till_date = trace_get_date(till_date)
+    log_info("(P)till-date: [%s]", till_date)
+
+    # fetch len
+    fetch_len = int(sai_conf_get2('boot', 'fetch_len'))
+    sai_fmt_set_fetch_len(fetch_len)
+    log_info("(P)fetch-len: [%d]", fetch_len)
+
 
     sql = 'select * from t_trc_user'
     log_info('user sql: [%s]', sql)
@@ -123,22 +175,22 @@ def production(_db):
         return -1
     else :
         df.set_index('user_id', inplace=True)
-        log_info('(P)user length: %d', len(df))
+        log_info('(P)df-user length: %d', len(df))
 
     for idx, row in df.iterrows():
-        log_info("-- %s --", idx)
+        log_info(">> uid: [%s]", idx)
         uid = idx 
         uname = row['user_name']
         umail = row['user_mail']
         enable = row['enable']
-        log_info(">> user: [%s], [%s], [%s]", uname, umail, enable)
+        log_info(">> detail: [%s], [%s], [%s]", uname, umail, enable)
         if enable == '1':
-            log_info('user [%s, %s] enable', uid, uname)
+            log_info('user [%s, %s] enabled', uid, uname)
         else:
             log_info('user [%s, %s] disabled, ignore', uid, uname)
             continue
 
-        trace_one_user(uid, _db)
+        trace_one_user(till_date, uid, _db)
 
     log_info('(P)mode: end')
 
@@ -146,23 +198,18 @@ def production(_db):
 
 
 
-def debug():
-    log_info('this is debug mode.')
-
-    return
-
-
-
 def work():
     #
     sai_load_conf2('trace.cfg')
 
-    # pd.set_option('display.max_columns', None)
-    # pd.set_option('display.max_rows', None)
-
-    debug_mode = sai_conf_get2('debug', 'enable')
+    debug_mode = sai_conf_get2('boot', 'debug')
     if debug_mode == 'yes':
-        log_debug('debug mode is YES.')
+        log_debug('DEBUG mode is YES.')
+
+    global g_photo_dir
+    g_photo_dir = "%s/project/tmp" % (os.getenv('HOME'))
+    log_info("photo-dir: [%s]", g_photo_dir)
+
 
     db = db_init()
     saiobj.g_db = db
@@ -183,6 +230,6 @@ if __name__=="__main__":
         log_info('workday, run')
         work()
 
-    log_debug("--end")
+    log_info("--end")
 
 # trace.py
